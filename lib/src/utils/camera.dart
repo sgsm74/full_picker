0cf492866ac5e4ac55f8deac9456c2f2bdc016a5
sgsm74 <s.gh4286@gmail.com>
2024-10-23 13:37:47 +0330

    Chore: refactor

--- lib/src/utils/camera.dart
+++ lib/src/utils/camera.dart
@@ -7,6 +7,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:full_picker/full_picker.dart';
+import 'package:full_picker/src/utils/image_preview.dart';
 import 'package:full_picker/src/utils/pl.dart';
 
 /// Custom Camera for Image and Video
@@ -624,99 +625,14 @@ class _CameraState extends State<Camera> with WidgetsBindingObserver {
                 color: Colors.white38,
                 borderRadius: BorderRadius.circular(20),
               ),
-              child: const Center(
+              child: Center(
                 child: Text(
-                  'ذخیره',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+                  globalFullPickerLanguage.ok,
+                  style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
                 ),
               ),
             ),
           ),
         ),
       );
-}
-
-class ImagePreviewView extends StatefulWidget {
-  const ImagePreviewView(this.file, {super.key});
-  final File file;
-
-  @override
-  // ignore: library_private_types_in_public_api
-  _ImagePreviewViewState createState() => _ImagePreviewViewState();
-}
-
-class _ImagePreviewViewState extends State<ImagePreviewView> {
-  @override
-  void initState() {
-    super.initState();
-    SystemChrome.setEnabledSystemUIMode(
-      SystemUiMode.manual,
-      overlays: <SystemUiOverlay>[
-        SystemUiOverlay.bottom,
-      ],
-    );
-  }
-
-  @override
-  void dispose() {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [SystemUiOverlay.top, SystemUiOverlay.bottom]);
-    super.dispose();
-  }
-
-  @override
-  Widget build(final BuildContext context) => Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.black,
-          elevation: 1,
-          leadingWidth: 100,
-          leading: GestureDetector(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: Container(
-              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-              child: const Text(
-                'بازگشت',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
-        ),
-        body: InteractiveViewer(
-          child: Container(
-            height: double.infinity,
-            width: double.infinity,
-            alignment: Alignment.center,
-            color: Colors.black,
-            child: SafeArea(
-              child: SingleChildScrollView(
-                reverse: true,
-                child: SizedBox(
-                  width: double.infinity,
-                  child: Hero(
-                    tag: widget.file.path,
-                    child: Image.file(
-                      widget.file,
-                      fit: BoxFit.contain,
-                      filterQuality: FilterQuality.high,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      );
-
-  Future decodeImage() async {
-    // ignore: prefer_typing_uninitialized_variables
-    final decodedImage = await decodeImageFromList(widget.file.readAsBytesSync());
-    return decodedImage.width.toDouble();
-  }
 }
---
+++ lib/src/utils/image_preview.dart
@@ -0,0 +1,90 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:full_picker/full_picker.dart';
+
+class ImagePreviewView extends StatefulWidget {
+  const ImagePreviewView(this.file, {super.key});
+  final File file;
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ImagePreviewViewState createState() => _ImagePreviewViewState();
+}
+
+class _ImagePreviewViewState extends State<ImagePreviewView> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.manual,
+      overlays: <SystemUiOverlay>[
+        SystemUiOverlay.bottom,
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [SystemUiOverlay.top, SystemUiOverlay.bottom]);
+    super.dispose();
+  }
+
+  @override
+  Widget build(final BuildContext context) => Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.black,
+          elevation: 1,
+          leadingWidth: 100,
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              child: Text(
+                globalFullPickerLanguage.cancel,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        body: InteractiveViewer(
+          child: Container(
+            height: double.infinity,
+            width: double.infinity,
+            alignment: Alignment.center,
+            color: Colors.black,
+            child: SafeArea(
+              child: SingleChildScrollView(
+                reverse: true,
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Hero(
+                    tag: widget.file.path,
+                    child: Image.file(
+                      widget.file,
+                      fit: BoxFit.contain,
+                      filterQuality: FilterQuality.high,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+
+  Future decodeImage() async {
+    // ignore: prefer_typing_uninitialized_variables
+    final decodedImage = await decodeImageFromList(widget.file.readAsBytesSync());
+    return decodedImage.width.toDouble();
+  }
+}
