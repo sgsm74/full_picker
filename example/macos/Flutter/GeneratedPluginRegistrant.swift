dbc3b6f8c7491f35ea62ed411e9c1d9a2d835d80
RamtinMoradi <ramtinmoradiiii@gmail.com>
2025-01-16 18:38:52 +0330

    Chore: remove light_compressor plugin and update dependencies in pubspec files

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import light_compressor
 import path_provider_foundation
 import record_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  LightCompressorPlugin.register(with: registry.registrar(forPlugin: "LightCompressorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
 }
